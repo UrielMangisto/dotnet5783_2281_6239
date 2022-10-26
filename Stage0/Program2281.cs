f0a8799c752208cdca07f5e0dc0f1ce7b8b5a7a9
Uriel Mangisto <umangist@g.jct.ac.il>
2022-10-26 11:29:21 +0300

    Initial code + Wellcome functions

---
+++ Stage0/Program2281.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Stage0 // Note: actual namespace depends on the project name.
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Wellcome6239();
+            Wellcome2281();
+            Console.ReadKey();
+        }
+
+        static partial void Wellcome6239();
+        private static void Wellcome2281()
+        {
+            string name;
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("{0}, wellcome to my first console application", name);
+        }
+    }
+}
