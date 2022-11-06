3798c6728440a0ea21d211485a3e064ee807e04f
Uriel Mangisto <umangist@g.jct.ac.il>
2022-11-06 22:37:01 +0200

    added the data source classes

--- DalList/DalOrder.cs
+++ DalList/DalOrder.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace Dal;
+﻿namespace Dal;
 
 public class DalOrder
 {
--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -7,10 +7,10 @@ internal static class DataSource
 {
     static readonly Random R = new Random(); 
     static internal Product[] products = new Product[50];
-    private static void createpProducts() { }
+    private static void addProductsToTheArray(Product p) { products[p.ID] = p; }
     static internal Order[] orders = new Order[100];
-    private static void createpOrder() { }
+    private static void addOrderToTheArray(Order order) { orders[order.ID] = order; }
     static internal OrderItem[] items = new OrderItem[200];
-    private static void createpItem() { }
-    private static void s_Initialize()
+    private static void addItemToTheArray(OrderItem item) { items[item.ID] = item; }
+    private static void s_Initialize() { }
 }
