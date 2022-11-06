3c82dad78f40e11687f0132362e855bf44231f78
דניאל בוני <danielboni058@gmail.com>
2022-11-06 20:33:57 +0200

    create new class in DalList

---
+++ DalList/DataSource.cs
@@ -0,0 +1,16 @@
+﻿
+using DO;
+
+namespace Dal;
+
+internal static class DataSource
+{
+    static readonly Random R = new Random(); 
+    static internal Product[] products = new Product[50];
+    private static void createpProducts() { }
+    static internal Order[] orders = new Order[100];
+    private static void createpOrder() { }
+    static internal OrderItem[] items = new OrderItem[200];
+    private static void createpItem() { }
+    private static void s_Initialize()
+}
