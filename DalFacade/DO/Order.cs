1cfca5fd2c7bed83ff83336ce1b8091ade5a173e
דניאל בוני <danielboni058@gmail.com>
2022-11-06 12:00:59 +0200

    change- order and product

--- DalFacade/DO/Order.cs
+++ DalFacade/DO/Order.cs
@@ -1,8 +1,18 @@
 ﻿
 
+using System.Diagnostics;
+using System.Xml.Linq;
+
 namespace DO;
 
 public struct Order
 {
     public int ID { get; set; }
+    public string CostumerName { get; set; }
+    public string CostumerEmail { get; set; }
+    public string CostumerAdress { get; set; }  
+    public DateTime OrderDate { get; set; } 
+    public DateTime ShipDate { get; set; }  
+    public DateTime DeliveryDate { get; set; }  
+
 }
