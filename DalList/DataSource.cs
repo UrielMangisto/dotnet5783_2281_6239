1d4842f43cf5584aeab1c9fece69c4ed44444723
Uriel Mangisto <umangist@g.jct.ac.il>
2022-11-07 18:51:54 +0200

    addProductsToTheArray

--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -8,18 +8,15 @@ internal static class DataSource
 
     static readonly Random R = new Random(); 
     static internal Product[] products = new Product[50];
-    private static void addProductsToTheArray(Product p) 
+   
+    static int currentSize = 0;
+    internal static void addProductsToTheArray(Product p)
     {
-        bool isadd=false;
-        for (int i = 0; i < products.Length&&!isadd; i++)
-        {
-            if (products[i].Name==null)//the place is empty
-            {
-                products[i] = p;
-                isadd=true; 
-            }
-        }
+        //if (something == (products.Length - 1))
+        //    Console.WriteLine("the array is full");
+        products[currentSize++] = p;
     }
+
     static internal Order[] orders = new Order[100];
     private static void addOrderToTheArray(Order order) { orders[order.ID] = order; }
     static internal OrderItem[] orderitems = new OrderItem[200];
