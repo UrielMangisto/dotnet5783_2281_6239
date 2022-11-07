419852ea43d2083c3759eb2b8129a830743031d6
דניאל בוני <danielboni058@gmail.com>
2022-11-07 19:38:22 +0200

    d

--- DalList/DalProduct.cs
+++ DalList/DalProduct.cs
@@ -1,5 +1,33 @@
-﻿namespace Dal;
+﻿using DO;
+
+namespace Dal;
 
 internal class DalProduct
 {
+    public void add(Product newProduct)
+    {
+        DataSource.addProductsToTheArray(newProduct);
+    }
+    public void delete(Product delProduct)
+      {
+        bool isdelate = false;
+        for(int i=0;i < DataSource.products.Length&&!isdelate;i++)
+        {
+            if(delProduct.ID==DataSource.products[i].ID)
+            {
+                isdelate = true;
+                DataSource.products[i].ID = 0;//if id=0 its deleated
+            }
+        }
+    }
+    public void update()
+    {
+
+    }
+    public void get()
+    {
+
+    }
+
 }
+
