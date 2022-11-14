88ff2b01594a6f6f13ac0149e0e49e4dce872b89
Uriel Mangisto <umangist@g.jct.ac.il>
2022-11-14 23:07:28 +0200

    some changes

---
+++ DalTest/main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DO;
+
+namespace Dal;
+
+internal class program
+{
+    public static void main(String[] args)
+    {
+        int choice;
+        do
+        {
+            Console.WriteLine(@$"press 0 to exit
+                                       1 to
+                                       2 to
+                                       3 to
+                                       4 to
+                                       5 to
+                                       6 to
+
+                                            ");
+            choice = int.Parse(Console.ReadLine());
+
+            switch(choice)
+            {
+                
+            }
+        }
+        while (choice == 0);
+    }
+}
