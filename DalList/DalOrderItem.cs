60ea5da6ba24a395a11d712ba45b5afd2b60d280
דניאל בוני <danielboni058@gmail.com>
2022-11-14 17:23:39 +0200

    build DalOrder

--- DalList/DalOrderItem.cs
+++ DalList/DalOrderItem.cs
@@ -1,18 +1,5 @@
-﻿using DO;
-using System;
-
-namespace Dal;
+﻿namespace Dal;
 
 internal class DalOrderitem
 {
-    public OrderItem getItemInOrder(int idOfProduct, int idOfOrder)
-    {
-        for(int i = 0; i < idOfOrder; i++)
-        {
-            if()
-            {
-
-            }
-        }
-    }
 }
