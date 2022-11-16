5dc09dc892f2b8794f082a932446591ad976bca1
דניאל בוני <danielboni058@gmail.com>
2022-11-16 12:22:28 +0200

    main

--- DalTest/main.cs
+++ DalTest/main.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DO;
+using Dal;
 
-namespace Dal;
-
-internal class program
+public class program
 {
     public static void main(String[] args)
     {
         int choice;
+        int temp;
+        Order order = new Order();
+        DalOrder dalOrder = new DalOrder();
+
+        OrderItem item = new OrderItem();
+        DalOrderitem dalItem = new DalOrderitem();
+
+        Product product = new Product();
+        DalProduct dalProduct = new DalProduct();
         do
         {
             Console.WriteLine(@$"press 0 to exit
@@ -35,22 +43,27 @@ internal class program
                     switch(choice)
                     {
                         case (int)Enums.OrderChoice.addOrder:
+                            order = orderInput(order);
 
+                            dalOrder.addNewOrder(order);
                             break;
                         case (int)Enums.OrderChoice.deleteOrder:
-                            
+                            Console.WriteLine("enter id order");
+                            dalOrder.deleteOrder(int.Parse(Console.ReadLine()));
                             break;
                         case (int)Enums.OrderChoice.updateOrder:
-
+                            order = orderInput(order);
+                            dalOrder.updateOrder(order);
                             break;
                         case (int)Enums.OrderChoice.getOrder:
-
+                            Console.WriteLine("enter id order");
+                            dalOrder.getOrder(int.Parse(Console.ReadLine()));
                             break;
                         case (int)Enums.OrderChoice.getAllOrder:
-
+                            dalOrder.getAllOrders();
                             break;
-                            default:    
-
+                            default:
+                            Console.WriteLine("ERROR");
                             break;
                     }
                     break;
@@ -68,24 +81,33 @@ internal class program
                     switch (choice)
                     {
                         case (int)Enums.orderItemChoise.addOrderItem:
-
+                            item = orderItempInput(item);
+                            dalItem.addItem(item);
                             break;
                         case (int)Enums.orderItemChoise.deleteOrderItem:
-
+                            Console.WriteLine("enter order item id");
+                            dalItem.deleteItem(int.Parse(Console.ReadLine()));
                             break;
                         case (int)Enums.orderItemChoise.updateOrderItem:
-
+                            item=orderItempInput(item);
+                            dalItem.updateItem(item);
                             break;
                         case (int)Enums.orderItemChoise.getOrderItem:
-
+                            Console.WriteLine("enter order item id");
+                            dalItem.getOrderItem(int.Parse(Console.ReadLine()));
                             break;
                         case (int)Enums.orderItemChoise.getSpecificItem:
-
+                            Console.WriteLine("enter product id");
+                            temp=int.Parse(Console.ReadLine());
+                            Console.WriteLine("enter order id");
+                            dalItem.specificItemGet(temp,int.Parse(Console.ReadLine()));
                             break;
                         case (int)Enums.orderItemChoise.getItemsByOrder:
-
+                            order= orderInput(order);
+                            dalItem.getItemsByOrder(order);
                             break;
                         case (int)Enums.orderItemChoise.getAllItems:
+                            dalItem.getAllItems(); 
 
                             break;
                         default:
@@ -131,5 +153,29 @@ internal class program
             }
         }
         while (choice != 0);
+    }
+
+    private static OrderItem orderItempInput(OrderItem item)
+    {
+        Console.WriteLine("enter product id");
+        item.ProductID = int.Parse(Console.ReadLine());
+        Console.WriteLine("enter order id");
+        item.OrderID = int.Parse(Console.ReadLine());
+        Console.WriteLine("enter order item price");
+        item.Price = int.Parse(Console.ReadLine());
+        Console.WriteLine("enter order item amount");
+        item.Amount = int.Parse(Console.ReadLine());
+        return item;
+    }
+
+    private static Order orderInput(Order order)
+    {
+        Console.WriteLine("enter your name");
+        order.CostumerName = Console.ReadLine();
+        Console.WriteLine("enter your address");
+        order.CostumerAddress = Console.ReadLine();
+        Console.WriteLine("enter your email");
+        order.CostumerEmail = Console.ReadLine();
+        return order;
     }
 }
