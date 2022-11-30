1f1de2a3fc88d5605e37974e8600e3468ca5da22
Uriel Mangisto <umangist@g.jct.ac.il>
2022-11-30 20:42:11 +0200

    experiment

--- BL/BlImplementation/BL.cs
+++ BL/BlImplementation/BL.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BlImplementation
-{
-    internal class BL
-    {
-    }
+namespace BlImplementation;
+
+internal class BL
+{ //public xrfd=1;
 }
